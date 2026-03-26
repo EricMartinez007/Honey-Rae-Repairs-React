8788f8fa51075a3306d0f2acaa3c887b9d9d4aad
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-26 10:19:49 -0500

    Added code for displaying customers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import  "./App.css"
+import { CustomerList } from "./components/customers/CustomerList"
 import { TicketList } from "./components/tickets/TicketList"
 
 export const App = () => {
   return (
     <div>
-    <TicketList />
+    {/*<TicketList /> */}
+    <CustomerList />
     </div>
   )
 }   
