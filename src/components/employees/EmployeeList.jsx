a4a734fbd480cf0e2a5abaaf1ef69e97b64be0a9
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-26 15:44:34 -0500

    Fixed a bug

--- src/components/employees/EmployeeList.jsx
+++ src/components/employees/EmployeeList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getStaffUsers } from "../../services/userService"
 import { User } from "../../users/User"
+import { Link } from "react-router-dom"
 import "./Employees.css"
 
 export const EmployeeList = () => {
@@ -15,7 +16,11 @@ export const EmployeeList = () => {
     return (
         <div className="employees">
             {employees.map(employeeObj => {
-                return <User user={employeeObj} />
+                return (
+                    <Link to={`/employees/${employeeObj.id}`} key={employeeObj.id}>
+                        <User user={employeeObj} />
+                    </Link>
+                )
             })}
         </div>
     )
