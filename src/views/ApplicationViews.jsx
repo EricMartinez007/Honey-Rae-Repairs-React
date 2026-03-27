03874ea65bddcc45e77198af862f1de18ab73a6b
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-27 17:14:56 -0500

    created two different routes: one for employees and one for customers

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,13 +1,7 @@
-import { Route, Outlet, Routes } from "react-router-dom"
-import { NavBar } from "../components/nav/NavBar"
-import { Welcome } from "../components/welcome/Welcome"
-import { EmployeeDetails } from "../components/employees/EmployeeDetails"
-import { EmployeeList } from "../components/employees/EmployeeList"
-import { TicketList } from "../components/tickets/TicketList"
-import { CustomerList } from "../components/customers/CustomerList"
-import { CustomerDetails } from "../components/customers/CustomerDetails"
 import { useEffect, useState } from "react"
 import { EmployeeForm } from "../components/forms/EmployeeForm"
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews"
 
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({})
@@ -19,29 +13,9 @@ export const ApplicationViews = () => {
     setCurrentUser(honeyUserObject)
   }, [])
 
-  return  (
-    <Routes>
-      <Route 
-        path="/" 
-        element={
-          <> 
-            <NavBar/> 
-            <Outlet/> 
-          </>
-        }
-      >
-        <Route index element={<Welcome/>}/>
-        <Route path="tickets" element={<TicketList currentUser={currentUser} />} />
-        <Route path="employees"  > 
-          <Route index element={<EmployeeList/>} />
-          <Route path=":employeeId" element={<EmployeeDetails/>} />
-        </Route>
-        <Route path="customers" > 
-          <Route index element={<CustomerList/>} />
-          <Route path=":customerId" element={<CustomerDetails/>} /> {/* /customers/:customerId */}
-        </Route>
-        <Route path="profile" element={<EmployeeForm currentUser={currentUser} />} />
-      </Route>
-    </Routes>
+  return  currentUser.isStaff ? (
+    <EmployeeViews currentUser={currentUser} />
+  ) : (
+    <CustomerViews currentUser={currentUser} />
   )
 }
