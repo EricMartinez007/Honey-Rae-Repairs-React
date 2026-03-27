730c9eb1b2957e02d8fa3afc8cf9693df7931361
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-27 14:23:24 -0500

    Added claim and close buttons to tickets for employees only

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -4,17 +4,20 @@ import "./Ticket.css"
 import { Ticket } from "./Ticket"
 import { TicketFilterBar } from "./TicketFilterBar"
 
-export const TicketList = () => {
+export const TicketList = ({ currentUser }) => {
     const [allTickets, setAllTickets] = useState([])
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
     const [searchTerm, setSearchTerm] = useState("")
+
+    const getAndSetTickets = () => {
+        getAllTickets().then(ticketsArray => {
+            setAllTickets(ticketsArray)
+        })
+    }
     
     useEffect(() => {
-        getAllTickets().then(ticketsArray => {
-        setAllTickets(ticketsArray)
-        console.log("tickets set!")
-    })
+        getAndSetTickets()
     }, []) // Only runs on initial render of component 
 
     useEffect(() => {
@@ -34,7 +37,7 @@ export const TicketList = () => {
     }, [searchTerm, allTickets])
 
     return (
-        <div className="tickets-container">
+    <div className="tickets-container">
         <h2>Tickets</h2>
         <TicketFilterBar 
             setShowEmergencyOnly={setShowEmergencyOnly} 
@@ -42,10 +45,16 @@ export const TicketList = () => {
         />
         <article className="tickets">
             {filteredTickets.map((ticketObj) => {
-            return <Ticket ticket={ticketObj} key={ticketObj.id}/>
-            
+            return (
+                <Ticket 
+                    ticket={ticketObj} 
+                    currentUser={currentUser} 
+                    getAndSetTickets = {getAndSetTickets}
+                    key={ticketObj.id}
+                />
+            )
             })}
         </article>
-        </div>
+    </div>
     )
 }
